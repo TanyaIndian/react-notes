72889db8c02d6ec6c067db49fb577ece59a6e7d8
TanyaIndian <97530331+TanyaIndian@users.noreply.github.com>
2024-02-04 11:24:55 +0530

    till pops

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import Video from './Components/Video';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      
+<Video text2 = "Nodejs" text1 = "React js"></Video>
+<Video text2 = "Html" text1 = "Css"></Video>
+    
+
+
     </div>
   );
 }
---
+++ my-app/src/Components/Video.js
@@ -0,0 +1,12 @@
+function Video({text1,text2})
+{
+    return (
+        <>
+        
+        <h1>{text1}</h1>
+        <p>{text2}</p>
+        </>
+    )
+}
+
+export default Video
